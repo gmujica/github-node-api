051d3586716a91ea77912f55d0127ec4d92185e7
gmujica <gregorymujica1993@gmail.com>
2022-04-20 16:02:53 -0500

    new structure implemented

---
+++ controllers/index.js
@@ -0,0 +1,61 @@
+const https = require('https');
+
+const GITHUB_TOKEN = '';
+
+const getUserInfo = async (req, res) => {
+    const user = req.params.user;
+    const options = {
+        hostname: 'api.github.com',
+        path: '/users/' + user,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
+        },
+        OAUth: GITHUB_TOKEN
+    }
+    https.get(options, (apiResponse) => {
+        apiResponse.pipe(res);
+    }).on('error', (e) => {
+        console.log(e);
+        res.status(500).send('Something wnent wrong!');
+    });
+};
+
+const getRepoInfo = async (req, res) => {
+    const user = req.params.user;
+    const reponame = req.params.reponame;
+    const options = {
+        hostname: 'api.github.com',
+        path: '/repos/' + user + '/' + reponame,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
+        },
+        OAUth: GITHUB_TOKEN
+    }
+    https.get(options, (apiResponse) => {
+        apiResponse.pipe(res);
+    }).on('error', (e) => {
+        console.log(e);
+        res.status(500).send('Something wnent wrong!');
+    });
+};
+
+const getCommitInfo = async (req, res) => {
+    const user = req.params.user;
+    const reponame = req.params.reponame;
+    const options = {
+        hostname: 'api.github.com',
+        path: '/repos/' + user + '/' + reponame + '/commits',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
+        },
+        OAUth: GITHUB_TOKEN
+    }
+    https.get(options, (apiResponse) => {
+        apiResponse.pipe(res);
+    }).on('error', (e) => {
+        console.log(e);
+        res.status(500).send('Something wnent wrong!');
+    });
+};
+
+module.exports = { getUserInfo, getRepoInfo, getCommitInfo };
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
 const app = express();
 
 const api = require('./routes/api');
 
-const cors = require('cors');
-
 app.use(bodyParser.json());
+dotenv.config();
 app.use(cors());
 
-app.use(function (req, res, next) {
+
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
@@ -18,10 +20,12 @@ app.use(function (req, res, next) {
 });
 
 app.use('/api', api);
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.send('Server is up and running!');
 });
 
-app.listen(5000, function () {
-    console.log('Server is listening');
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Backend server is running on http://localhost:${port}`);
 });
