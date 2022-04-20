332c6654e70ee42b61c8207d362bbf2c7ee54a6d
gmujica <gregorymujica1993@gmail.com>
2022-04-20 15:26:53 -0500

    repository info endpoin added

--- routes/api.js
+++ routes/api.js
@@ -18,7 +18,26 @@ router.get('/github/userinfo/:user', async (req, res) => {
     }).on('error', (e) => {
         console.log(e);
         res.status(500).send('Something wnent wrong!');
-    })
-})
+    });
+});
+
+router.get('/github/repoinfo/:user/:reponame', async (req, res) => {
+    const user = req.params.user;
+    const reponame = req.params.reponame;
+    const options = {
+        hostname: 'api.github.com',
+        path: '/repos/' + user + '/' + reponame,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1521.3 Safari/537.36'
+        },
+        OAUth: "<paste your token here>"
+    }
+    https.get(options, function (apiResponse) {
+        apiResponse.pipe(res);
+    }).on('error', (e) => {
+        console.log(e);
+        res.status(500).send('Something wnent wrong!');
+    });
+});
 
 module.exports = router;
